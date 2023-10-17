71873e11f4818dde8a8e92911d4562dad36f37ad
chitranjanji <chitranjanharode@gmail.com>
2023-10-17 11:37:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$('#overlay h2').textillate({
+    in: {
+        effect: 'fadeInLeftBig',
+        delayScale: 1,
+    }
+});
+$('#overlay h3').textillate({
+    in: {
+        effect: 'fadeInLeftBig',
+        delayScale: 1,
+    }
+});
+ 
